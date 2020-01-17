3d4141ba6b93d6b0bb410fc71cafc413e996e220
ztq <zhangtongquan1013@126.com>
2020-01-17 14:48:05 +0800

    添加数据结构的工具类Utils

---
+++ app/src/main/java/com/ztq/sdk/data_structure/Utils.java
@@ -0,0 +1,59 @@
+package com.ztq.sdk.data_structure;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: ztq
+ * Date: 2020/1/17 10:48
+ * Description: 数据结构中的工具类
+ */
+public class Utils {
+    private static final String TAG = "noahedu.Utils";
+
+    /**
+     * 获取第index个丑数
+     * 丑数概念：我们把只包含因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含因子7。
+     * 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第index个丑数。
+     * @param index
+     * @return
+     */
+    public static int getUglyNumber(int index) {
+        if (index <= 0) {
+            return -1;
+        }
+        int twoCount = 0;
+        int threeCount = 0;
+        int fiveCount = 0;
+        List<Integer> uglyList = new ArrayList<>();
+        uglyList.add(1);
+        for (int i = 0; i < index - 1; i++) {
+            while (true) {
+                if (uglyList.get(twoCount) * 2 > uglyList.get(uglyList.size() - 1)) {
+                    break;
+                }
+                twoCount++;
+            }
+            while (true) {
+                if (uglyList.get(threeCount) * 3 > uglyList.get(uglyList.size() - 1)) {
+                    break;
+                }
+                threeCount++;
+            }
+            while (true) {
+                if (uglyList.get(fiveCount) * 5 > uglyList.get(uglyList.size() - 1)) {
+                    break;
+                }
+                fiveCount++;
+            }
+
+            int a = Math.min(uglyList.get(twoCount) * 2, uglyList.get(threeCount) * 3);
+            int b = Math.min(a, uglyList.get(fiveCount) * 5);
+            uglyList.add(b);
+        }
+        Log.v(TAG, "第" + index + "个丑数为：" + uglyList.get(index - 1));
+        return uglyList.get(index - 1);
+    }
+}
