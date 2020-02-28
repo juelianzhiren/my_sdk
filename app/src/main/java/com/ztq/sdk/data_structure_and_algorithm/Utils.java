3b6298af4df2b071a55949cba4134436be9af19c
ztq <zhangtongquan1013@126.com>
2020-02-28 09:14:29 +0800

    Utils中添加方法：礼物的最大价值

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -2377,4 +2377,36 @@ public class Utils {
         }
         return false;
     }
+
+    /**
+     * 礼物的最大价值
+     * 在一个m×n的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于0）。
+     * 你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格直到到达棋盘的右下角。
+     * 给定一个棋盘及其上面的礼物，请计算你最多能拿到多少价值的礼物
+     * @param values
+     * @return
+     */
+    public static int maxValueOfGifts(int[][] values) {
+        if (values == null || values.length <= 0 || values[0].length <= 0) {
+            return 0;
+        }
+        int rows = values.length;
+        int cols = values[0].length;
+        int[] maxValue = new int[cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int left = 0;
+                int up = 0;
+                if (i > 0) {
+                    up = maxValue[j];
+                }
+                if (j > 0) {
+                    left = maxValue[j - 1];
+                }
+                maxValue[j] = Math.max(up, left) + values[i][j];
+                Log.v(TAG, "i = " + i + "; j = " + j + "; maxValue = " + maxValue[j] + "; up = " + up + "; left = " + left);
+            }
+        }
+        return maxValue[cols - 1];
+    }
 }
