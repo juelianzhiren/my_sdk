f8dba8af9169fa0f12384f89f9c99f3252ce0930
ztq <zhangtongquan1013@126.com>
2020-03-02 23:16:48 +0800

    Utils添加方法：左旋转字符串、翻转单词顺序

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -2582,4 +2582,62 @@ public class Utils {
         }
         kthNodeHelper(node.getRightNode(), k, cache);
     }
+
+    /**
+     * 翻转单词顺序
+     * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+     * 为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+     * @param chars
+     * @return
+     */
+    public static String reverseSentence(char[] chars) {
+        if (chars == null || chars.length <= 0) {
+            return String.valueOf(chars);
+        }
+        //翻转整个句子
+        reverseSb(chars, 0, chars.length - 1);
+        //翻转单词（指针指向单词的第一个和最后一个）
+        int start = 0;
+        int end = 0;
+        while (start < chars.length) {
+            while (end < chars.length && chars[end] != ' ') {
+                end++;
+            }
+            reverseSb(chars, start, end - 1);
+            start = ++end;
+        }
+        return String.valueOf(chars);
+    }
+
+    private static void reverseSb(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
+     * 左旋转字符串
+     * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+     * 请定义一个函数实现字符串左旋转操作的功能。
+     * 比如输入字符串"abcdefg"和数字2，该函数将返回左旋转2位得到的结果"cdefgab"。
+     * @param chars
+     * @param n
+     * @return
+     */
+    public static String leftRotateString(char[] chars, int n) {
+        if (chars == null || chars.length <= 0) {
+            return String.valueOf(chars);
+        }
+        if (n <= 0 || n > chars.length) {
+            return String.valueOf(chars);
+        }
+        reverseSb(chars, 0, n - 1);
+        reverseSb(chars, n, chars.length - 1);
+        reverseSb(chars, 0, chars.length - 1);
+        return String.valueOf(chars);
+    }
 }
