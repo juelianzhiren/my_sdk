f389c6b8994a63512966754b2d8ec19d4d15b73d
ztq <zhangtongquan1013@126.com>
2020-03-03 09:34:46 +0800

    Utils中添加方法：滑动窗口的最大值

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -2,6 +2,7 @@ package com.ztq.sdk.data_structure_and_algorithm;
 
 import android.util.Log;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -2639,5 +2640,41 @@ public class Utils {
         reverseSb(chars, n, chars.length - 1);
         reverseSb(chars, 0, chars.length - 1);
         return String.valueOf(chars);
+    }
+
+    /**
+     * 滑动窗口的最大值
+     * 给定一个数组和滑动窗口的大小，请找出所有滑动窗口里的最大值。
+     * 例如，如果输入数组{2, 3, 4, 2, 6, 2, 5, 1}及滑动窗口的大小3，那么一共存在6个滑动窗口，它们的最大值分别为{4, 4, 6, 6, 6, 5}
+     * @param num
+     * @param size
+     * @return
+     */
+    public static ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> max = new ArrayList<Integer>();
+        if (num == null || num.length <= 0 || size <= 0 || size > num.length) {
+            return max;
+        }
+        ArrayDeque<Integer> indexDeque = new ArrayDeque<Integer>();
+
+        for (int i = 0; i < size - 1; i++) {
+            while (!indexDeque.isEmpty() && num[i] > num[indexDeque.getLast()]) {
+                indexDeque.removeLast();
+            }
+            indexDeque.addLast(i);
+        }
+        System.out.println(indexDeque.size());
+        for (int i = size - 1; i < num.length; i++) {
+            while (!indexDeque.isEmpty() && num[i] > num[indexDeque.getLast()]) {
+                indexDeque.removeLast();
+            }
+
+            if (!indexDeque.isEmpty() && (i - indexDeque.getFirst()) >= size) {
+                indexDeque.removeFirst();
+            }
+            indexDeque.addLast(i);
+            max.add(num[indexDeque.getFirst()]);
+        }
+        return max;
     }
 }
