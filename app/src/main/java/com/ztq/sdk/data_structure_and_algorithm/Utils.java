13584b7da17e8094d48f38e6b0d75aef11c65bfe
ztq <zhangtongquan1013@126.com>
2020-03-20 11:26:44 +0800

    Utils添加方法：寻找两个有序数组的中位数

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -2787,4 +2787,59 @@ public class Utils {
         }
         return B;
     }
+
+    /**
+     * 寻找两个有序数组的中位数
+     * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+     * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+     * 你可以假设 nums1 和 nums2 不会同时为空。
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int size = len1 + len2;
+        if (size % 2 == 1) {
+            return findKth(nums1, 0, len1, nums2, 0, len2, size / 2 + 1);
+        } else {
+            return (findKth(nums1, 0, len1, nums2, 0, len2, size / 2) + findKth(nums1, 0, len1, nums2, 0, len2, size / 2 + 1)) / 2;
+        }
+    }
+
+    /**
+     * @param nums1
+     * @param start1
+     * @param len1
+     * @param nums2
+     * @param start2
+     * @param len2
+     * @param k k在方法中的真正含义为“还需要找到多少个数才能达到k个
+     * @return
+     */
+    private static double findKth(int[] nums1, int start1, int len1, int[] nums2, int start2, int len2, int k) {
+        if (len1 - start1 > len2 - start2) {        // 传进来的时候统一让短的数组为nums1
+            return findKth(nums2, start2, len2, nums1, start1, len1, k);
+        }
+        System.out.println("len1 = " + len1 + "; start1 = " + start1 + "; len2 = " + len2 + "; start2 = " + start2 + "; k = " + k);
+        if (len1 - start1 == 0) {           // 表示nums1已经全部加入前K个了，第k个为nums2[k - 1];
+            return nums2[k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+            // k==1表示已经找到第k-1小的数，下一个数为两个数组start开始的最小值
+        }
+        int p1 = start1 + Math.min(len1 - start1, k / 2); // p1和p2记录当前需要比较的那个位
+        int p2 = start2 + k - (p1 - start1);
+
+        System.out.println("nums1[" + (p1 - 1) + "] = " + nums1[p1 - 1] + "; nums2[" + (p2 - 1) + "] = " + nums2[p2 - 1] + "; (p1 + p2) = " + (p1 + p2));
+        if (nums1[p1 - 1] < nums2[p2 - 1]) {
+            return findKth(nums1, p1, len1, nums2, start2, len2, k - p1 + start1);
+        } else if (nums1[p1 - 1] > nums2[p2 - 1]) {
+            return findKth(nums1, start1, len1, nums2, p2, len2, k - p2 + start2);
+        } else {
+            return nums1[p1 - 1];
+        }
+    }
 }
