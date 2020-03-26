761c824238f64c7212d25aed9ece5c450c56325f
zhangtongquan1013@126.com <zhangtongquan1013@126.com>
2020-03-26 10:16:24 +0800

    Utils中添加方法：字符串转换整数 (atoi)

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -2971,4 +2971,56 @@ public class Utils {
             return (int) res;
         }
     }
+
+    /**
+     * 字符串转换整数 (atoi)
+     * 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+     * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+     * 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+     * 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+     * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+     * 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+     * @param str
+     * @return
+     */
+    public static int myAtoi(String str) {
+        if (com.ztq.sdk.utils.Utils.isNullOrNil(str)) {
+            return 0;
+        }
+        str = str.trim(); // 删除字符串头尾空格
+        if (str.length() == 0) {
+            return 0;
+        }
+        int flag = 1; // 符号位标识
+        int rev = 0; // 数值（无符号）
+        int edge = Integer.MAX_VALUE / 10;               // 判断数值是否超过范围的边界线，这样写可以节省时间
+        if (str.charAt(0) == '-') {
+            flag = -1;
+            str = str.substring(1, str.length());                  // 跳过符号位，可不写第二参数
+        } else if (str.charAt(0) == '+') {
+            str = str.substring(1, str.length());                 // 跳过符号位，可不写第二参数
+        } else if (!(str.charAt(0) >= '0' && str.charAt(0) <= '9')) { // 如果开始非空字符不为符号或数字，则直接返回0
+            return 0;
+        }
+        for (char s : str.toCharArray()) {
+            if (s >= '0' && s <= '9') {
+                int n = s - '0'; // 计算字符代表值
+                if (rev >= edge) { // 超过边界情况较少，故该判断写于外侧
+                    if (flag == 1) {
+                        if (rev > edge || n > 7) {             // Integer.MAX_VALUE=2147483647,最后一位为7
+                            return Integer.MAX_VALUE;
+                        }
+                    } else {
+                        if (rev > edge || n > 8) {           // Integer.MIN_VALUE=-2147483648,最后一位为8
+                            return Integer.MIN_VALUE;
+                        }
+                    }
+                }
+                rev = rev * 10 + n;
+            } else {
+                break;
+            }
+        }
+        return rev * flag;
+    }
 }
