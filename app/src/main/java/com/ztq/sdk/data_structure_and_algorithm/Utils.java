51f94211c92de3880556e6f4f582b6c25138d555
zhangtongquan1013@126.com <zhangtongquan1013@126.com>
2020-04-03 14:12:06 +0800

    Utils添加方方法：移除元素

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -3203,4 +3203,46 @@ public class Utils {
         head.next = reverseKGroup(tail, k);
         return newHead;
     }
+
+    /**
+     * 删除排序数组中的重复项
+     * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int back = 0;
+        for (int front = 1; front < nums.length; front++) {
+            if (nums[back] != nums[front]) {
+                back++;
+                nums[back] = nums[front];
+            }
+        }
+        return back + 1;
+    }
+
+    /**
+     * 移除元素
+     * 给定一个数组和一个值val，你需要原地移除所有值等于val的元素，并返回移除后的数组长度
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElements(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for(int j = 0; j < nums.length; j++) {
+            if (nums[j] == val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
 }
