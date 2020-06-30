b1e41be6d2d730fab0a95b8100e88dd94fa02b59
zhangtongquan1013@126.com <zhangtongquan1013@126.com>
2020-06-30 15:23:08 +0800

    格式化代码

--- app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
+++ app/src/main/java/com/ztq/sdk/data_structure_and_algorithm/Utils.java
@@ -3082,32 +3082,32 @@ public class Utils {
     }
 
     // 求解两个字符号的最长公共子串
-    public static String maxSubstring(String strOne, String strTwo){
+    public static String maxSubstring(String strOne, String strTwo) {
         // 参数检查
-        if(strOne==null || strTwo == null){
+        if (strOne == null || strTwo == null) {
             return null;
         }
-        if(strOne.equals("") || strTwo.equals("")){
+        if (strOne.equals("") || strTwo.equals("")) {
             return null;
         }
         // 二者中较长的字符串
         String max = "";
         // 二者中较短的字符串
         String min = "";
-        if(strOne.length() < strTwo.length()){
+        if (strOne.length() < strTwo.length()) {
             max = strTwo;
             min = strOne;
-        } else{
+        } else {
             max = strTwo;
             min = strOne;
         }
         String current = "";
         // 遍历较短的字符串，并依次减少短字符串的字符数量，判断长字符是否包含该子串
-        for(int i=0; i<min.length(); i++){
-            for(int begin=0, end=min.length()-i; end<=min.length(); begin++, end++){
+        for (int i = 0; i < min.length(); i++) {
+            for (int begin = 0, end = min.length() - i; end <= min.length(); begin++, end++) {
                 current = min.substring(begin, end);
                 System.out.println("current = " + current + "; max = " + max + "; min = " + min + "; i = " + i);
-                if(max.contains(current)){
+                if (max.contains(current)) {
                     return current;
                 }
             }
