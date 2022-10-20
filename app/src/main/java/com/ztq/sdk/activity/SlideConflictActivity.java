87bc990b124056e627817d8fcce880b02292d76f
zhangtq <zhangtongquan1013@126.com>
2022-10-20 11:59:10 +0800

    添加滑动冲突demo

--- app/src/main/java/com/ztq/sdk/activity/SlideConflictActivity.java
+++ app/src/main/java/com/ztq/sdk/activity/SlideConflictActivity.java
@@ -10,6 +10,8 @@ import android.widget.RelativeLayout;
 
 import com.ztq.sdk.R;
 import com.ztq.sdk.widget.HorizontalEx;
+import com.ztq.sdk.widget.HorizontalEx2;
+import com.ztq.sdk.widget.ListViewEx;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,6 +59,7 @@ public class SlideConflictActivity extends BaseActivity{
             @Override
             public void onClick(View v) {
                 mMainRL.removeAllViews();
+                generateData3();
             }
         });
 
@@ -103,5 +106,45 @@ public class SlideConflictActivity extends BaseActivity{
 
         RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         mMainRL.addView(mHorizontalEx, params1);
+    }
+
+    private void generateData3(){
+        List<String> data1 = new ArrayList();
+        List<String> data2 = new ArrayList();
+        List<String> data3 = new ArrayList();
+        for(int i = 0; i < 20; i++) {
+            data1.add("列表1--" + (i + 1));
+            data2.add("列表2--" + (i + 1));
+            data3.add("列表3--" + (i + 1));
+        }
+        showInnerHVData(data1, data2, data3);
+    }
+
+    public void showInnerHVData(List<String> data1, List<String> data2, List<String> data3) {
+        HorizontalEx2 mHorizontalEx2 = new HorizontalEx2(mContext);
+
+        ListViewEx listView1 = new ListViewEx(mContext);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, data1);
+        listView1.setAdapter(adapter1);
+        listView1.setmHorizontalEx2(mHorizontalEx2);
+
+        ListViewEx listView2 = new ListViewEx(mContext);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, data2);
+        listView2.setAdapter(adapter2);
+        listView2.setmHorizontalEx2(mHorizontalEx2);
+
+        ListViewEx listView3 = new ListViewEx(mContext);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, data3);
+        listView3.setAdapter(adapter3);
+        listView3.setmHorizontalEx2(mHorizontalEx2);
+
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        mHorizontalEx2.addView(listView1, params);
+        mHorizontalEx2.addView(listView2, params);
+        mHorizontalEx2.addView(listView3, params);
+
+        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mMainRL.addView(mHorizontalEx2, params1);
     }
 }
