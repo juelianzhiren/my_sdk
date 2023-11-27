833fe1fd47cb206224a6553dec72d2eaee155a0a
zhangtq <zhangtongquan1013@126.com>
2023-11-27 13:53:02 +0800

    添加MediaExtractor逻辑页面

--- AudioVideoRelated/src/main/java/com/demo/audiovideorelated/activity/MediaExtractorMuxerActivity.java
+++ AudioVideoRelated/src/main/java/com/demo/audiovideorelated/activity/MediaExtractorMuxerActivity.java
@@ -9,11 +9,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 
 import com.demo.audiovideorelated.R;
 import com.noahedu.noah_permissions.OnPermissionCallback;
 import com.noahedu.noah_permissions.Permission;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -32,6 +35,20 @@ public class MediaExtractorMuxerActivity extends BaseActivity {
 
         setContentView(R.layout.activity_media_exractor_muxer);
         checkPermissions();
+
+        findViewById(R.id.media_extractor_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exactorMedia();
+            }
+        });
+
+        findViewById(R.id.media_muxer_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                muxerMedia();
+            }
+        });
     }
 
     private void checkPermissions() {
@@ -41,7 +58,7 @@ public class MediaExtractorMuxerActivity extends BaseActivity {
                 @Override
                 public List<String> permissions() {
                     List<String> pers = new ArrayList<>();
-                    pers.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    pers.add(Manifest.permission.READ_EXTERNAL_STORAGE);
                     pers.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
                     return pers;
                 }
@@ -129,5 +146,146 @@ public class MediaExtractorMuxerActivity extends BaseActivity {
         mMediaMuxer.release();
 
         return true;
+    }
+
+    private MediaExtractor mediaExtractor = new MediaExtractor();
+    private void exactorMedia() {
+        FileOutputStream videoOutputStream = null;
+        FileOutputStream audioOutputStream = null;
+        try {
+            //分离的视频文件
+            File videoFile = new File(SDCARD_PATH, "output_video.mp4");
+            //分离的音频文件
+            File audioFile = new File(SDCARD_PATH, "output_audio.mp3");
+            videoOutputStream = new FileOutputStream(videoFile);
+            audioOutputStream = new FileOutputStream(audioFile);
+            //源文件
+            mediaExtractor.setDataSource(SDCARD_PATH + "/ss.mp4");
+            //信道总数
+            int trackCount = mediaExtractor.getTrackCount();
+            int audioTrackIndex = -1;
+            int videoTrackIndex = -1;
+            for (int i = 0; i < trackCount; i++) {
+                MediaFormat trackFormat = mediaExtractor.getTrackFormat(i);
+                String mineType = trackFormat.getString(MediaFormat.KEY_MIME);
+                //视频信道
+                if (mineType.startsWith("video/")) {
+                    videoTrackIndex = i;
+                }
+                //音频信道
+                if (mineType.startsWith("audio/")) {
+                    audioTrackIndex = i;
+                }
+            }
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(500 * 1024);
+            //切换到视频信道
+            mediaExtractor.selectTrack(videoTrackIndex);
+            while (true) {
+                int readSampleCount = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleCount < 0) {
+                    break;
+                }
+                //保存视频信道信息
+                byte[] buffer = new byte[readSampleCount];
+                byteBuffer.get(buffer);
+                videoOutputStream.write(buffer);
+                byteBuffer.clear();
+                mediaExtractor.advance();
+            }
+            Log.v(TAG, "extractor video success");
+            //切换到音频信道
+            mediaExtractor.selectTrack(audioTrackIndex);
+            while (true) {
+                int readSampleCount = mediaExtractor.readSampleData(byteBuffer, 0);
+                Log.v(TAG, "readSampleCount = " + readSampleCount);
+                if (readSampleCount < 0) {
+                    break;
+                }
+                //保存音频信息
+                byte[] buffer = new byte[readSampleCount];
+                byteBuffer.get(buffer);
+                audioOutputStream.write(buffer);
+                byteBuffer.clear();
+                mediaExtractor.advance();
+            }
+            Log.v(TAG, "extractor audio success");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            mediaExtractor.release();
+            try {
+                videoOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void muxerMedia() {
+        MediaMuxer mediaMuxer = null;
+        mediaExtractor = new MediaExtractor();
+        int videoIndex = -1;
+        try {
+            mediaExtractor.setDataSource(SDCARD_PATH + "/ss.mp4");
+            int trackCount = mediaExtractor.getTrackCount();
+            for (int i = 0; i < trackCount; i++) {
+                MediaFormat trackFormat = mediaExtractor.getTrackFormat(i);
+                String mimeType = trackFormat.getString(MediaFormat.KEY_MIME);
+                // 取出视频的信号
+                if (mimeType.startsWith("video/")) {
+                    videoIndex = i;
+                }
+            }
+            //切换道视频信号的信道
+            mediaExtractor.selectTrack(videoIndex);
+            MediaFormat trackFormat = mediaExtractor.getTrackFormat(videoIndex);
+            mediaMuxer = new MediaMuxer(SDCARD_PATH + "/output_video.mp4", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            //追踪此信道
+            int trackIndex = mediaMuxer.addTrack(trackFormat);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 500);
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            mediaMuxer.start();
+            long videoSampleTime;
+            //获取每帧的之间的时间
+            {
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                //skip first I frame
+                if (mediaExtractor.getSampleFlags() == MediaExtractor.SAMPLE_FLAG_SYNC)
+                    mediaExtractor.advance();
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                long firstVideoPTS = mediaExtractor.getSampleTime();
+                mediaExtractor.advance();
+                mediaExtractor.readSampleData(byteBuffer, 0);
+                long SecondVideoPTS = mediaExtractor.getSampleTime();
+                videoSampleTime = Math.abs(SecondVideoPTS - firstVideoPTS);
+                Log.d("fuck", "videoSampleTime is " + videoSampleTime);
+            }
+            //重新切换此信道，不然上面跳过了3帧,造成前面的帧数模糊
+            mediaExtractor.unselectTrack(videoIndex);
+            mediaExtractor.selectTrack(videoIndex);
+            while (true) {
+                //读取帧之间的数据
+                int readSampleSize = mediaExtractor.readSampleData(byteBuffer, 0);
+                if (readSampleSize < 0) {
+                    break;
+                }
+                mediaExtractor.advance();
+                bufferInfo.size = readSampleSize;
+                bufferInfo.offset = 0;
+                bufferInfo.flags = mediaExtractor.getSampleFlags();
+                bufferInfo.presentationTimeUs += videoSampleTime;
+                //写入帧的数据
+                mediaMuxer.writeSampleData(trackIndex, byteBuffer, bufferInfo);
+            }
+            //release
+            mediaMuxer.stop();
+            mediaExtractor.release();
+            mediaMuxer.release();
+
+            Log.e(TAG, "finish");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
