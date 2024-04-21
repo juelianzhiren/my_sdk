44eaa62bb3eb18fda22e70df3158036c5772478d
zhangtongquan1013@126.com <zhangtongquan1013@126.com>
2024-04-21 23:29:39 +0800

    添加反射相关逻辑

---
+++ app/src/main/java/com/ztq/sdk/inject/EventType.java
@@ -0,0 +1,15 @@
+package com.ztq.sdk.inject;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventType {
+    Class listenerType();
+
+    String listenerSetter();
+
+}
